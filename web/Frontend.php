cebd594365288ff3f2bf505a9040db9dfca8624c
mgeissen <matt.geis97@gmail.com>
2018-03-30 17:41:20 +0200

    fix wrong include

--- web/Frontend.php
+++ web/Frontend.php
@@ -43,7 +43,6 @@ class Frontend{
 
     private function renderHead(){
         include "resources/html/header.php";
-        include_once "html/header.html";
         if(file_exists("../../customHeader.php")){
             include_once "../../customHeader.php";
         }
