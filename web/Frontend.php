1fe20e2408ee007a3f1318346365d2b86839ef74
mgeissen <matt.geis97@gmail.com>
2018-06-28 20:16:11 +0200

    test without checking of txt file

--- web/Frontend.php
+++ web/Frontend.php
@@ -87,7 +87,7 @@ class Frontend{
     }
 
     private function renderBody(){
-        if(isset($_POST["submit"]) && strpos($this->uploadFile , "txt")){
+        if(isset($_POST["submit"])){
             include_once "resources/html/stats.php";
             ?>
             <script>
